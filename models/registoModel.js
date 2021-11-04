b6dac0102cf55318205f06c59a4a421c8f239fe2
AC_Marvel <gil_1_10@hotmail.com>
2021-11-04 17:51:13 +0000

    Login, Registo mas estão incompleto

---
+++ models/registoModel.js
@@ -0,0 +1,19 @@
+var mysql = require('./connection').pool;
+
+module.exports.registar = function (obj, callback, next){
+    mysql.getConnection(function(err, conn) {
+        if(err){
+            conn.release();
+            next(err);
+        }
+        else conn.query('INSERT INTO Utilizador(Nome, Username, Email, Senha) VALUES(?,?,?,?)', [obj.Nome, obj.Username, obj.email, obj.Senha], function(err, rows){
+            conn.release();
+            if(!(rows.length === 0)){
+                callback({code: 200, status: 'Ok'},rows)
+            }
+            else {
+                callback({code: 401, status: "Username ou Senha incorrecta!"}, null);
+            }
+        })
+    })
+}
