c8b22c09fc9b2da0c6b5d993d61a20c88aa39afe
AC_Marvel <gil_1_10@hotmail.com>
2021-11-21 15:21:31 +0000

    mostrar o mapa na pagina...

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
 var mapa
+var coordPopup = L.popup()
+
 
 window.onload = function(){
     document.getElementById('Nome').innerHTML = localStorage.getItem('Nome')
--- public/javascripts/mapa.js
+++ public/javascripts/mapa.js
@@ -4,7 +4,7 @@ function carregarMapa(idElement){
     var mapa = L.map(idElement)
     var attribution = '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors';
     var tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var tiles = L.tileLayer(tileUrl, {attribution});
+    var tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(mapa);
     /**Coordenadas de localização de Lisboa */
     mapa.setView([38.7071, -9.13549], 6);
